cf007f7fa553f12629366e6f91564f34650e2340
Michael Spradlin <ms703316@ohio.edu>
2023-10-19 13:14:17 -0400

    Version 0.9.9 Character additions

--- OrangeJuiceModMaker/MainWindow.xaml.cs
+++ OrangeJuiceModMaker/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ImageMagick;
 using Microsoft.VisualBasic.FileIO;
